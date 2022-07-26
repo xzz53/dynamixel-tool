5171aa856066954947c93b72dca2c358d0ca2e17
Mikhail Rudenko <mike.rudenko@gmail.com>
2022-07-26 18:03:41 +0300

    cleanup stale files

---
+++ dynamixel-tool/src/bin/dynamixel-stress.rs
@@ -0,0 +1,26 @@
+use anyhow::Result;
+
+use dynamixel_utils::port;
+use dynamixel_utils::protocol::{self, ProtocolVersion};
+use env_logger::TimestampPrecision;
+
+fn main() -> Result<()> {
+    let mut port = port::open_port("auto", 1000000, false)?;
+    let mut proto_box = protocol::make_protocol(ProtocolVersion::V1, port.as_mut(), 0);
+
+    env_logger::Builder::from_env(env_logger::Env::default())
+        .format_timestamp(Some(TimestampPrecision::Millis))
+        .format_target(false)
+        .init();
+
+    loop {
+        proto_box.write(1, 20, &[0, 0])?;
+        proto_box.write(1, 20, &[0, 0])?;
+        proto_box.write(1, 20, &[0, 0])?;
+        proto_box.write(1, 20, &[0, 0])?;
+        proto_box.write(1, 20, &[0, 0])?;
+        // std::thread::sleep_ms(10);
+        let r1 = proto_box.read(51, 18, 2)?;
+        println!("{:?}", r1);
+    }
+}
