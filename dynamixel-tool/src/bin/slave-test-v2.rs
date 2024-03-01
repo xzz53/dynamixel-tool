7867e0c4c9a6661d3a1e56e37ccaaddca92ce698
Mikhail Rudenko <mike.rudenko@gmail.com>
2024-03-01 03:50:48 +0300

    slave-test: better logging

--- dynamixel-tool/src/bin/slave-test-v2.rs
+++ dynamixel-tool/src/bin/slave-test-v2.rs
@@ -1,18 +1,19 @@
 use std::convert::TryInto;
 
 use anyhow::Result;
+use env_logger::TimestampPrecision;
+use log::info;
 
 use dynamixel_lib::port::{self};
 use dynamixel_lib::protocol::slave::{make_async_protocol, Opcode};
 use dynamixel_lib::protocol::ProtocolVersion;
-use env_logger::TimestampPrecision;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let mut port = port::open_port_async("auto", 1000000, false)?;
     let mut proto = make_async_protocol(ProtocolVersion::V2, &mut port);
     let mut regs = [0u8; 65536];
-    let my_id = 1u8;
+    let my_id = 10u8;
 
     env_logger::Builder::from_env(env_logger::Env::default())
         .format_timestamp(Some(TimestampPrecision::Millis))
@@ -21,9 +22,9 @@ async fn main() -> Result<()> {
 
     loop {
         let r1 = proto.recv_instruction().await?;
-        println!("{:?}", r1);
+        info!("{:?}", r1);
         if r1.id != my_id {
-            println!("skipping id={}", r1.id);
+            info!("skipping id={}", r1.id);
             continue;
         }
 
@@ -33,14 +34,14 @@ async fn main() -> Result<()> {
             }
             Opcode::Read => {
                 if r1.data.len() != 4 {
-                    println!("error: malformed read");
+                    info!("error: malformed read");
                     continue;
                 }
                 let addr = u16::from_le_bytes(r1.data[0..2].try_into().unwrap()) as usize;
                 let size = u16::from_le_bytes(r1.data[2..4].try_into().unwrap()) as usize;
 
                 if addr + size > regs.len() {
-                    println!("error: bad size ({size}) for address {addr}");
+                    info!("error: bad size ({size}) for address {addr}");
                     continue;
                 }
 
@@ -50,14 +51,14 @@ async fn main() -> Result<()> {
             }
             Opcode::Write => {
                 if r1.data.len() < 3 {
-                    println!("error: malformed write");
+                    info!("error: malformed write");
                     continue;
                 }
                 let addr = u16::from_le_bytes(r1.data[0..2].try_into().unwrap()) as usize;
                 let size = r1.data.len() - 2;
 
                 if addr + size > regs.len() {
-                    println!("error: bad size ({size}) for address {addr}");
+                    info!("error: bad size ({size}) for address {addr}");
                     continue;
                 }
 
@@ -66,7 +67,7 @@ async fn main() -> Result<()> {
                 proto.send_status(my_id, 0, &[]).await?;
             }
             op => {
-                println!("{op:?} not supported");
+                info!("{op:?} not supported");
                 proto.send_status(my_id, 1u8 << 6, &[]).await?;
             }
         }
--- dynamixel-tool/src/bin/slave-test.rs
+++ dynamixel-tool/src/bin/slave-test.rs
@@ -4,24 +4,25 @@ use dynamixel_lib::port::{self};
 use dynamixel_lib::protocol::slave::{make_async_protocol, Opcode};
 use dynamixel_lib::protocol::ProtocolVersion;
 use env_logger::TimestampPrecision;
+use log::{info, error};
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let mut port = port::open_port_async("auto", 1000000, false)?;
     let mut proto = make_async_protocol(ProtocolVersion::V1, &mut port);
     let mut regs = [0u8; 256];
-    let my_id = 1u8;
+    let my_id = 10u8;
 
     env_logger::Builder::from_env(env_logger::Env::default())
-        .format_timestamp(Some(TimestampPrecision::Millis))
+        .format_timestamp(Some(TimestampPrecision::Micros))
         .format_target(false)
         .init();
 
     loop {
         let r1 = proto.recv_instruction().await?;
-        println!("{:?}", r1);
+        info!("{:?}", r1);
         if r1.id != my_id {
-            println!("skipping id={}", r1.id);
+            info!("skipping id={}", r1.id);
             continue;
         }
 
@@ -31,40 +32,42 @@ async fn main() -> Result<()> {
             }
             Opcode::Read => {
                 if r1.data.len() != 2 {
-                    println!("error: malformed read");
+                    error!("error: malformed read");
                     continue;
                 }
                 let addr = r1.data[0] as usize;
                 let size = r1.data[1] as usize;
 
                 if addr + size > regs.len() {
-                    println!("error: bad size ({size}) for address {addr}");
+                    error!("error: bad size ({size}) for address {addr}");
                     continue;
                 }
 
                 proto
                     .send_status(my_id, 0, &regs[addr..addr + size])
                     .await?;
+                info!("read complete");
             }
             Opcode::Write => {
                 if r1.data.len() < 2 {
-                    println!("error: malformed write");
+                    error!("error: malformed write");
                     continue;
                 }
                 let addr = r1.data[0] as usize;
                 let size = r1.data.len() - 1;
 
                 if addr + size > regs.len() {
-                    println!("error: bad size ({size}) for address {addr}");
+                    error!("error: bad size ({size}) for address {addr}");
                     continue;
                 }
 
                 regs[addr..addr + size].copy_from_slice(&r1.data[1..1 + size]);
 
                 proto.send_status(my_id, 0, &[]).await?;
+                info!("write complete");
             }
             op => {
-                println!("{op:?} not supported");
+                error!("{op:?} not supported");
                 proto.send_status(my_id, 1u8 << 6, &[]).await?;
             }
         }
